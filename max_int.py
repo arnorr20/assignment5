bf5eec1826cb53ff480cdf3911115772a0bbfe2e
AAR <aar@aar.is>
2020-09-03 13:17:24 +0000

    Algorithm written for max_int

---
+++ max_int.py
@@ -0,0 +1,12 @@
+# The max integer algorithm is going to repeatedly ask the user to input an
+# integer and but stop if the integer gets negative.
+# When the integer gets negative and the program stops it
+# is going to print out the highest integer the user inputed over that session.
+
+# Repeatedly ask user for a integer
+# if integer is positive
+# store integer in a varible max_int
+# if it is higher than the number stored in max_int
+# if user input is a negative integer
+# stop asking user to input a number
+# and output the number stored in max_int variable
