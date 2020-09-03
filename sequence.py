1b98c11ab4683b231ea3bb033c47939f438ba555
AAR <aar@aar.is>
2020-09-03 18:26:59 +0000

    Náði lokins að klára þetta

--- sequence.py
+++ sequence.py
@@ -5,12 +5,16 @@
 # For as many times as user input
 # run the sequence that prints n every time
 # if n gets higher than 3 then
-# n is the sum of previous 3 numbers
+# n is the sum of previous 3 printed numbers
+# for exapmle: 1+2+3=6 then 2+3+6=11
 
 n = int(input("Enter the length of the sequence: ")) # Do not change this line
-
-for i in range(1,n):
+a,b,c=1,2,3
+for i in range(1,n+1):
+    
     if i < 4:
         print(i)
     else:
-        print('eitthvað')
+        sum_num = a+b+c
+        print(sum_num)
+        a,b,c = b,c,sum_num
