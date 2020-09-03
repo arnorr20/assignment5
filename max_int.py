aabcbc4c78519e3bc66b0fd42b7fdac0881aaab6
AAR <aar@aar.is>
2020-09-03 13:30:00 +0000

    Algorithm complete

--- max_int.py
+++ max_int.py
@@ -18,9 +18,11 @@ num_int = int(input("Input a number: "))    # Do not change this line
 max_int = 0
 
 while num_int >= 0:
+
+    # Store integer in max_int if the integer is higher than stored value
     if num_int > max_int:
         max_int = num_int
-
+    
     num_int = int(input("Input a number: "))
 
 print("The maximum is", max_int)    # Do not change this line
