09dfa70efcf770454cdbb23670811a791751bdc2
AAR <aar@aar.is>
2020-09-03 14:03:17 +0000

    Algorithm written for sequence

---
+++ sequence.py
@@ -0,0 +1,8 @@
+# Design an algorithm that generates the first n numbers 
+# in the following sequence:; 1, 2, 3, 6, 11, 20, 37, ___, ___, ___, 
+
+# User inputs an integer
+# For as many times as user input
+# run the sequence that prints n every time
+# if n gets higher than 3 then
+# n is the sum of previous 3 numbers
